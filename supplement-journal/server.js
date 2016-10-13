92528393e3de7e1cb2c6f97664cc79014812be87
atourianski <a.tourianski@gmail.com>
2016-10-13 13:49:04 -0400

    formatting

--- supplement-journal/server.js
+++ supplement-journal/server.js
@@ -13,24 +13,24 @@ http.createServer(function (request, response) {
         var contentType = 'text/html';
 	    switch (extname) {
 		            case '.js':
-				                contentType = 'text/javascript';
-						            break;
-							            case '.css':
-							                contentType = 'text/css';
-									            break;
-										            case '.json':
-										                contentType = 'application/json';
-												            break;
-													            case '.png':
-													                contentType = 'image/png';
-															            break;      
-																            case '.jpg':
-																                contentType = 'image/jpg';
-																		            break;
-																			            case '.wav':
-																			                contentType = 'audio/wav';
-																					            break;
-																						        }
+           contentType = 'text/javascript';
+	            break;
+	            case '.css':
+           contentType = 'text/css';
+	            break;
+	            case '.json':
+           contentType = 'application/json';
+	            break;
+	            case '.png':
+	    contentType = 'image/png';
+	            break;      
+	            case '.jpg':
+            contentType = 'image/jpg';
+	            break;
+	            case '.wav':
+  	    contentType = 'audio/wav';
+	            break;
+        }
 
 	        fs.readFile(filePath, function(error, content) {
 			        if (error) {
@@ -39,16 +39,16 @@ http.createServer(function (request, response) {
 										                        response.writeHead(200, { 'Content-Type': contentType });
 													                    response.end(content, 'utf-8');
 															                    });
-									                }
-						                else {
-									                response.writeHead(500);
+			                }
+	            else {
+  	                  response.writeHead(500);
 											                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
 													                response.end(); 
 															            }
 		        }
-				        else {
-						            response.writeHead(200, { 'Content-Type': contentType });
-							                response.end(content, 'utf-8');
+		        else {
+		            response.writeHead(200, { 'Content-Type': contentType });
+         	                response.end(content, 'utf-8');
 									        }
 		    });
 
