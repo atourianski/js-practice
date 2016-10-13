6ba6f736dbd95745816612dc46fcc30dabff1098
atourianski <a.tourianski@gmail.com>
2016-10-13 13:41:24 -0400

    new server

--- supplement-journal/server.js
+++ supplement-journal/server.js
@@ -1,17 +1,56 @@
-const http = require('http')  
-const port = 3000
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
 
-const requestHandler = (request, response) => {  
-	  console.log(request.url)
-		    response.end('Hello Node.js Server!')
-}
+http.createServer(function (request, response) {
+	    console.log('request starting...');
 
-const server = http.createServer(requestHandler)
+	        var filePath = '.' + request.url;
+		    if (filePath == './')
+	        filePath = './index.html';
 
-	server.listen(port, (err) => {  
-		  if (err) {
-			      return console.log('something bad happened', err)
-		  }
+    var extname = path.extname(filePath);
+        var contentType = 'text/html';
+	    switch (extname) {
+		            case '.js':
+				                contentType = 'text/javascript';
+						            break;
+							            case '.css':
+							                contentType = 'text/css';
+									            break;
+										            case '.json':
+										                contentType = 'application/json';
+												            break;
+													            case '.png':
+													                contentType = 'image/png';
+															            break;      
+																            case '.jpg':
+																                contentType = 'image/jpg';
+																		            break;
+																			            case '.wav':
+																			                contentType = 'audio/wav';
+																					            break;
+																						        }
 
-		    console.log(`server is listening on ${port}`)
-	})
+	        fs.readFile(filePath, function(error, content) {
+			        if (error) {
+					            if(error.code == 'ENOENT'){
+							                    fs.readFile('./404.html', function(error, content) {
+										                        response.writeHead(200, { 'Content-Type': contentType });
+													                    response.end(content, 'utf-8');
+															                    });
+									                }
+						                else {
+									                response.writeHead(500);
+											                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+													                response.end(); 
+															            }
+		        }
+				        else {
+						            response.writeHead(200, { 'Content-Type': contentType });
+							                response.end(content, 'utf-8');
+									        }
+		    });
+
+}).listen(8125);
+console.log('Server running at http://127.0.0.1:8125/');
