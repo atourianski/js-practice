e1ae34a3783069a488fdbaf378ac71f1af584bc0
atourianski <a.tourianski@gmai.com>
2017-06-26 03:55:22 +0000

    words now added to array before being scanned

--- word-finder/app.js
+++ word-finder/app.js
@@ -1,13 +1,28 @@
-var sjw = [/injustice/g, /postmodern/g, /inequality/g, /inequity/g, /Marx/g, /diversity/g, /multiculturalism/g, /patriarchy/g, /racism/g, /sexism/g, /cisgender/g];
-
-
-var elements =  document.body.getElementsByTagName("*");
-	for(i=0;i<elements.length;i++){
-		for(x in sjw){
-			var scan = elements[i].innerText.match(sjw[x])
-				if(scan != null){
-					console.log(scan)
-				}
+var sjw = [/injustice/gi, /postmodern/gi, /inequality/gi, /inequity/gi, /Marx/gi, /diversity/gi, /multiculturalism/gi, /patriarchy/gi, /racism/gi, /sexism/gi, /cisgender/gi];
+var storage = [];
+var elements = document.body.getElementsByTagName("*");
+for(i in elements){
+	try{	
+		storage.push(elements[i].lastChild.data)
+	}
+	catch(err){
+		storage.push(elements[i].innerText)
+		if(elements[i].innerText === undefined){
+			console.log("well shit")	
 		}
 	}
-	
+
+}
+
+
+
+var readyToScan = storage.join();
+
+for(x in sjw){
+	var scan = readyToScan.match(sjw[x]);
+		if(scan != null){
+			console.log(scan)
+		}
+}
+
+
